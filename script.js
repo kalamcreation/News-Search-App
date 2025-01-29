089246c654e7bf7726ad067b0a1c82f67dc5d9ed
kalamcreation <mdabulkalamosman@gmail.com>
2025-01-29 22:43:40 +0600

    4th commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apikey = '5f3d97a59dcd4400b0fd42e1efca05aa'
+const apiKey = "5f3d97a59dcd4400b0fd42e1efca05aa"
 
 const blogContainer = document.getElementById("blog-container");
 
@@ -6,22 +6,43 @@ const blogContainer = document.getElementById("blog-container");
 
 async function fetchRandomNews(){
     try{
-        const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=$(apikey)';
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=$(apiKey)`;
         const response = await fetch (apiUrl);
         const data = await response.json();
-        console.log(data);
-
+        //console.log(data);
+        return data.articles;
     }catch(error){
         console.error("Error Fetching Random News", error);
         return[];
     }
 }
+ 
+function displayBlogs(articles){
+    blogContainer.innerHTML = "";
+    articles.forEach((article)=>{
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt = article.title;
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+        const description = document.createElement ("p");
+        description.textContent = article.description;
 
-async()=>{
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogContainer.appendChild(blogCard);
+    });
+}
+
+(async () => {
     try{
         const articles = await fetchRandomNews();
-        console.log(articles);
+        displayBlogs(articles);
+        //console.log(articles);
     }catch(error){
         console.error("Error fetching random news", error);
     }
-}
+})();
