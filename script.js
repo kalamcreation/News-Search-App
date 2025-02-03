ebe28923f0db76a07c52ec3d11ee2ba7f1ce086b
kalamcreation <mdabulkalamosman@gmail.com>
2025-02-04 00:01:59 +0600

    4th commit

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const apiKey = "5f3d97a59dcd4400b0fd42e1efca05aa"
 
 const blogContainer = document.getElementById("blog-container");
-
+const searchField = document.getElementById ("blog-container");
+const searchButton = document.getElementById ("search-button");
 
 
 async function fetchRandomNews(){
     try{
         const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=$(apiKey)`;
+        const response = await fetch (apiUrl);
+        const data = await response.json();
+        //console.log(data);
+        return data.articles;
+    }catch(error){
+        console.error("Error Fetching Random News", error);
+        return[];
+    }
+}
+
+searchButton.addEventListener("click", async () => {
+    const query = searchField.value.trim()
+    if(query !== ""){
+        try{
+            const articles = await fetchNewsQuery (query)
+            displayBlogs(articles)
+
+        }catch(error){
+            console.log("Error fetching news by query", error)
+        }
+    }
+}) 
+
+function fetchNewsQuery(query){
+    try{
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=$(apiKey)`;
         const response = await fetch (apiUrl);
         const data = await response.json();
         //console.log(data);
@@ -26,13 +53,23 @@ function displayBlogs(articles){
         img.src = article.urlToImage;
         img.alt = article.title;
         const title = document.createElement("h2");
-        title.textContent = article.title;
+        const.truncatedTitle = article.title.length > 30
+        ? article.title.slice(0, 30) + "...." 
+        : article.title;
+        title.textContent = truncatedTitle;
         const description = document.createElement ("p");
+        const.truncatedDes = article.description.length > 120
+        ? article.description.slice(0, 120) + "...." 
+        : article.description;
+        title.textContent = truncatedDes;
         description.textContent = article.description;
 
         blogCard.appendChild(img);
         blogCard.appendChild(title);
         blogCard.appendChild(description);
+        blogCard.addEventListener('click', ()=> {
+            window.open(article.url, "_blank");
+        });
         blogContainer.appendChild(blogCard);
     });
 }
